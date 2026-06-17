317184ef5f20a64090cdc7eaafec8c0a9b4c113e
Ivan <130054944+igrzetic@users.noreply.github.com>
2026-06-17 13:26:07 +0200

    Implemented Hero scroll animation

--- src/components/Hero copy.jsx
+++ src/components/Hero copy.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
@@ -10,9 +10,15 @@ const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
-    const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
 
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
@@ -41,12 +47,13 @@ const Hero = () => {
         },
       })
       .to(".right-leaf", { y: 200 }, 0)
-      .to(".left-leaf", { y: -200 }, 0);
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
     const endValue = isMobile ? "120% top" : "bottom top";
 
-    const tl = gsap.timeline({
+    let tl = gsap.timeline({
       scrollTrigger: {
         trigger: "video",
         start: startValue,
@@ -67,6 +74,7 @@ const Hero = () => {
     <>
       <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
+
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
@@ -79,6 +87,8 @@ const Hero = () => {
         />
 
         <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
@@ -102,10 +112,10 @@ const Hero = () => {
       <div className="video absolute inset-0">
         <video
           ref={videoRef}
-          src="/videos/output.mp4"
           muted
           playsInline
           preload="auto"
+          src="/videos/output.mp4"
         />
       </div>
     </>
